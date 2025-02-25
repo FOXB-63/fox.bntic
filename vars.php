7350cb81e45d0029e01d2b666fb3e0005739258f
BETAR AMINE NGARNDIGBAYE <bet.am.ngar@gmail.com>
2025-02-25 23:40:03 +0100

    Update and rename index.html to vars.php

---
+++ vars.php
@@ -0,0 +1,17 @@
+<?php
+
+define("SRVNAME", "localhost");
+define("SRVBDD", "fox");
+define("SRVUTS", "root");
+define("SRVPWD", "");
+define("SRVPORT", "");
+
+//Autres variables
+define("SITETITRE", "FOX.B-NTIC");
+define("CENTRELOCAL", "DOUMBERT 3, Moundou");
+define("EMAIL", "foxbntic@gmail.com");
+define("TELEPHONE", "+235 63 90 56 84 / 97 64 37 69");
+define("SLOGAN", "Nous formons pour le bien être du monde NTIC");
+
+
+$indpage = '';
