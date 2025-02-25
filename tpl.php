d35e129c7357d9a8120bc8665fc29edc0537f4b5
BETAR AMINE NGARNDIGBAYE <bet.am.ngar@gmail.com>
2025-02-25 23:41:10 +0100

    Create tpl.php

---
+++ tpl.php
@@ -0,0 +1,165 @@
+
+<!DOCTYPE html>
+<html lang="en">
+    <?php include_once 'regions/head.php'; ?>
+    <body>
+
+        <!-- Spinner Start -->
+        <div id="spinner" class="show w-100 vh-100 bg-white position-fixed translate-middle top-50 start-50  d-flex align-items-center justify-content-center">
+            <div class="spinner-grow text-primary" role="status"></div>
+        </div>
+        <!-- Spinner End -->
+        <?php include_once 'regions/navbar.php'; ?>
+        <!-- Hero Start -->
+        <div id="rechslog" class="container-fluid py-5 mb-5 hero-header">
+            <div  class="container py-5">
+                <div class="row g-5 align-items-center">
+                    <div id="slogan" class="col-md-12 col-lg-7">
+                        <h1 class="mb-5 display-5 text-primary"><?php print SLOGAN; ?></h1>
+                    </div> <div id="rech" class="col-md-12 col-lg-7">
+                        <div class="position-relative mx-auto">
+                            <input class="form-control border-2 border-secondary w-75 py-3 px-4 rounded-pill" type="text" placeholder="Chercher">
+                            <button type="submit" class="btn btn-primary border-2 border-secondary py-3 px-4 position-absolute rounded-pill text-white h-100" style="top: 0; right: 25%;">Rechercher</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Hero End -->
+
+
+       
+        
+        <!-- Banner Section Start-->
+        <div class="container-fluid banner bg-black my-1">
+                <div class="text-center">
+                    <div class="py-4">
+                        <h1 class="display-3 text-white">Canal +</h1>
+                        <p class="mb-4 text-secondary">Réabonnement : Access, Evasion, Access+, Evasion+, Tout canal.
+                         <span class="fw-bold text-white">Vente de décodeurs et accessoires</span></p>
+                    </div>
+                </div>
+        </div>
+        
+        
+        <div id="seceqp" class="container-fluid testimonial py-5"><div class="container py-5">
+            <div class="testimonial-header text-center"><h1 class="text-center text-primary">Notre équipe</h1></div>
+            
+            
+            <div class="owl-carousel testimonial-carousel">
+                    <div class="testimonial-item img-border-radius bg-light rounded p-4">
+                        <div class="position-relative">
+                            <i class="fa fa-quote-right fa-2x text-secondary position-absolute" style="bottom: 30px; right: 0;"></i>
+                            <div class="mb-4 pb-4 border-bottom border-secondary">
+                                <p class="mb-0">Licence en Sciences En Informatique et Télécommunications</p>
+                            </div>
+                            <div class="d-flex align-items-center flex-nowrap">
+                                <div class="bg-secondary rounded">
+                                    <img src="./images/fox.png" class="img-fluid rounded" style="width: 100px; height: 100px;" alt="">
+                                </div>
+                                <div class="ms-4 d-block">
+                                    <h4 class="text-dark">BETAR AMINE NGARNDIGBAYE</h4>
+                                    <p class="m-0 pb-3">PRESIDENT FONDATEUR</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="testimonial-item img-border-radius bg-light rounded p-4">
+                        <div class="position-relative">
+                            <i class="fa fa-quote-right fa-2x text-secondary position-absolute" style="bottom: 30px; right: 0;"></i>
+                            <div class="mb-4 pb-4 border-bottom border-secondary">
+                                <p class="mb-0">Licence en Informatique et Télécommunications</p>
+                            </div>
+                            <div class="d-flex align-items-center flex-nowrap">
+                                <div class="bg-secondary rounded">
+                                    <img src="./images/ALAIN.jpg" class="img-fluid rounded" style="width: 100px; height: 100px;" alt="">
+                                </div>
+                                <div class="ms-4 d-block">
+                                    <h4 class="text-dark">DAOUNG ALAIN</h4>
+                                    <p class="m-0 pb-3">DIRECTEUR GENERAL</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="testimonial-item img-border-radius bg-light rounded p-4">
+                        <div class="position-relative">
+                            <i class="fa fa-quote-right fa-2x text-secondary position-absolute" style="bottom: 30px; right: 0;"></i>
+                            <div class="mb-4 pb-4 border-bottom border-secondary">
+                                <p class="mb-0">Licence en Informatique et Télécommunications</p>
+                            </div>
+                            <div class="d-flex align-items-center flex-nowrap">
+                                <div class="bg-secondary rounded">
+                                    <img src="img/testimonial-1.jpg" class="img-fluid rounded" style="width: 100px; height: 100px;" alt="">
+                                </div>
+                                <div class="ms-4 d-block">
+                                    <h4 class="text-dark">ALYO FIRMIN</h4><p class="m-0 pb-3">TRESORIER</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>  
+            </div>
+        </div>
+
+        <div id="seceqp" class="container-fluid testimonial py-5"><div class="container py-5">
+            <div class="testimonial-header text-center"><h1 class="text-center text-primary">Nos Partenaires</h1></div>
+            
+            
+            <div class="owl-carousel testimonial-carousel">
+                    <div class="testimonial-item img-border-radius bg-light rounded p-4">
+                        <div class="position-relative">
+                            <i class="fa fa-quote-right fa-2x text-secondary position-absolute" style="bottom: 30px; right: 0;"></i>
+                            <div class="mb-4 pb-4 border-bottom border-secondary">
+                                <p class="mb-0"></p>
+                            </div>
+                            <div class="d-flex align-items-center flex-nowrap">
+                                <div class="bg-secondary rounded">
+                                    <img src="img/testimonial-1.jpg" class="img-fluid rounded" style="width: 100px; height: 100px;" alt="">
+                                </div>
+                                <div class="ms-4 d-block">
+                                    <h4 class="text-dark"></h4>
+                                    <p class="m-0 pb-3"></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="testimonial-item img-border-radius bg-light rounded p-4">
+                        <div class="position-relative">
+                            <i class="fa fa-quote-right fa-2x text-secondary position-absolute" style="bottom: 30px; right: 0;"></i>
+                            <div class="mb-4 pb-4 border-bottom border-secondary">
+                                <p class="mb-0"></p>
+                            </div>
+                            <div class="d-flex align-items-center flex-nowrap">
+                                <div class="bg-secondary rounded">
+                                    <img src="img/testimonial-1.jpg" class="img-fluid rounded" style="width: 100px; height: 100px;" alt="">
+                                </div>
+                                <div class="ms-4 d-block">
+                                    <h4 class="text-dark"></h4>
+                                    <p class="m-0 pb-3"></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="testimonial-item img-border-radius bg-light rounded p-4">
+                        <div class="position-relative">
+                            <i class="fa fa-quote-right fa-2x text-secondary position-absolute" style="bottom: 30px; right: 0;"></i>
+                            <div class="mb-4 pb-4 border-bottom border-secondary">
+                                <p class="mb-0"></p>
+                            </div>
+                            <div class="d-flex align-items-center flex-nowrap">
+                                <div class="bg-secondary rounded">
+                                    <img src="img/testimonial-1.jpg" class="img-fluid rounded" style="width: 100px; height: 100px;" alt="">
+                                </div>
+                                <div class="ms-4 d-block">
+                                    <h4 class="text-dark"></h4><p class="m-0 pb-3">Technicien</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>  
+            </div>
+        </div>
+        
+        
+        <?php include_once 'regions/foot.php'; ?>
+    </body>
+
+</html>
